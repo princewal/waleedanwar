87a5ebcde3f861bdc82ca526c9651dc90101bc7d
princewal <princewal@hotmail.com>
2024-04-07 00:33:40 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let body = document.body
+let dayNight = document.querySelector(".dayNight")
+
+dayNight.onclick = function () {
+  body.classList.toggle("night")
+}
+
+let typingEffect = new Typed("#text", {
+  strings: ["Waleed", "a Developer", "a Coder", "a Lead"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 2500,
+})
+
+function fillUpPortfolio(portfolio) {
+  console.log("portfolio", portfolio)
+}
+
+async function fetchPortfolio() {
+  const response = await fetch("/js/projects.json")
+  const portfolio = await response.json()
+  return portfolio
+}
+
+window.addEventListener("DOMContentLoaded", function (e) {
+  fetchPortfolio().then((json) => {
+    fillUpPortfolio(json)
+  })
+})
